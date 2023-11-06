3e41a3fe39b6b6e5f2d25a3ff5b8210f7636c9d0
David BANITONGWA <davidbanitongwa@gmail.com>
2023-11-06 22:41:44 +0100

    update

--- nlp_test.py
+++ nlp_test.py
@@ -1,12 +1,8 @@
 from nltk.corpus import twitter_samples
-import nltk
-import os
 
 # Get the current working directory
 current_path = os.getcwd()
 
-#positive_tweets = twitter_samples.strings('data/twitter/positive_tweets.json')
-#negative_tweets = twitter_samples.strings('data/twitter/negative_tweets.json')
-#text = twitter_samples.strings('data/twitter/tweets.20150430-223406.json')
-
-nltk.download('punkt', download_dir=current_path)
+positive_tweets = twitter_samples.strings('data/twitter/positive_tweets.json')
+negative_tweets = twitter_samples.strings('data/twitter/negative_tweets.json')
+text = twitter_samples.strings('data/twitter/tweets.20150430-223406.json')
