ed9fa75ce57805901c9bbac9c84b6056598bc363
David BANITONGWA <davidbanitongwa@gmail.com>
2023-11-07 16:55:42 +0100

    update 1 file and delete 2 files

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-import nltk
+import pandas as pd
+import numpy as np
+import seaborn as sns
 
-nltk.download('twitter_samples')
+df = pd.read_csv('data/emotion_dataset.csv')
+
+#print(df.head())
+
+#print(df['Emotion'].value_counts())
+
+print(sns.countplot(x='Emotion', data=df))
