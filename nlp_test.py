61e24e375552be4b7977b78bce63cf1539c39c07
David BANITONGWA <davidbanitongwa@gmail.com>
2023-11-07 10:38:47 +0100

    update

--- nlp_test.py
+++ nlp_test.py
@@ -1,8 +1,10 @@
 from nltk.corpus import twitter_samples
+from nltk.tag import pos_tag
+from nltk.stem.wordnet import WordNetLemmatizer
 
 # Get the current working directory
 #current_path = os.getcwd()
-
+"""
 positive_tweets = twitter_samples.strings('positive_tweets.json')
 negative_tweets = twitter_samples.strings('negative_tweets.json')
 text = twitter_samples.strings('tweets.20150430-223406.json')
@@ -10,3 +12,25 @@ text = twitter_samples.strings('tweets.20150430-223406.json')
 tweet_tokens = twitter_samples.tokenized('positive_tweets.json')
 
 print(tweet_tokens)
+"""
+
+def lemmatize_sentence(tokens):
+    lemmatizer = WordNetLemmatizer()
+    lemmatized_sentence = []
+    for word, tag in pos_tag(tokens):
+        if tag.startswith('NN'):
+            pos = 'n'
+        elif tag.startswith('VB'):
+            pos = 'v'
+        else:
+            pos = 'a'
+        lemmatized_sentence.append(lemmatizer.lemmatize(word, pos))
+    return lemmatized_sentence
+
+
+
+tweet_tokens = twitter_samples.tokenized('positive_tweets.json')
+
+print(lemmatize_sentence(tweet_tokens[0]))
+
+
---
+++ nltk_download.py
@@ -0,0 +1,14 @@
+import nltk
+
+# **** 1
+
+#nltk.download('twitter_samples')
+
+# **** 2
+
+#nltk.download('punkt')
+
+# **** 3
+
+#nltk.download('wordnet')
+#nltk.download('averaged_perceptron_tagger')
